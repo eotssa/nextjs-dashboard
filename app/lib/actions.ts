2bc44881abc10092e3a34be8317d6d6ae908f427
eotsso <107133870+eotsso@users.noreply.github.com>
2023-12-15 01:04:59 -0500

    Update actions, data, sidenav, and login form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,13 +5,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-// const FormSchema = z.object({
-//   id: z.string(),
-//   customerId: z.string(),
-//   amount: z.coerce.number(),
-//   status: z.enum(['pending', 'paid']),
-//   date: z.string(),
-// });
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -114,13 +109,30 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
